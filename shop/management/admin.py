26f7820433fe97d13f0599c21ffa9ca68d28d60f
Lex-23 <Liverpoolsmile23@gmail.com>
2021-03-01 01:15:17 +0300

    add first models

--- shop/management/admin.py
+++ shop/management/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Category, Product, Store
+from .models import (Category, Product, Stock,
+                     ProdStock, Store, OrderStore)
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -23,11 +24,41 @@ class ProductAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 
 
-class StoreAdmin(admin.ModelAdmin):
-    fields = 'name', 'product',
-    search_fields = 'name', 'product',
+class StockAdmin(admin.ModelAdmin):
+    fields = 'name', 'description',
+    search_fields = 'name',
     list_display = 'name', 'id',
-    list_filter = 'name', 'product',
+    list_filter = 'name',
+
+
+admin.site.register(Stock, StockAdmin)
+
+
+class ProdStockAdmin(admin.ModelAdmin):
+    fields = 'unique_number', 'stock', 'product', 'balance',
+    search_fields = 'stock', 'product', 'unique_number'
+    list_display = 'id', 'unique_number', 'stock', 'product', 'balance',
+    list_filter = 'stock', 'product',
+
+
+admin.site.register(ProdStock, ProdStockAdmin)
+
+
+class StoreAdmin(admin.ModelAdmin):
+    fields = 'name', 'description',
+    search_fields = 'name',
+    list_display = 'name', 'id',
+    list_filter = 'name',
 
 
 admin.site.register(Store, StoreAdmin)
+
+
+class OrderStoreAdmin(admin.ModelAdmin):
+    fields = 'store', 'order', 'count',
+    search_fields = 'store', 'order',
+    list_display = 'id', 'store', 'order', 'count',
+    list_filter = 'store',
+
+
+admin.site.register(OrderStore, OrderStoreAdmin)
--- shop/management/models.py
+++ shop/management/models.py
@@ -15,8 +15,10 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    name = models.CharField(max_length=200, verbose_name='Product')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE,
+                                 related_name='products')
+    name = models.CharField(max_length=200, verbose_name='product')
     description = models.TextField(blank=True, null=True)
 
     class Meta:
@@ -26,16 +28,57 @@ class Product(models.Model):
         return self.name
 
 
+class Stock(models.Model):
+    """модель Склады"""
+    name = models.CharField(max_length=200, verbose_name='stock_name')
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Stock'
+
+    def __str__(self):
+        return self.name
+
+
+class ProdStock(models.Model):
+    """модель наличия товара на складе, каждая строка - уникальная"""
+    unique_number = models.PositiveIntegerField(unique=True)
+    stock = models.ForeignKey(Stock,
+                              on_delete=models.DO_NOTHING,
+                              related_name='stocks_prod')
+    product = models.ForeignKey(Product,
+                                on_delete=models.DO_NOTHING,
+                                related_name='products_stock')
+    balance = models.PositiveBigIntegerField(default=0)
+
+    class Meta:
+        ordering = ('stock', 'product')
+        verbose_name = 'Products_in_stock'
+
+    def __str__(self):
+        return str(self.unique_number)
+
+
 class Store(models.Model):
-    product = models.ManyToManyField(Product, related_name='products')
-    name = models.CharField(max_length=200, verbose_name='Store_name')
-    # id_store_prod = models.PositiveIntegerField(unique=True, auto_created=True)
+    """модель Магазины"""
+    name = models.CharField(max_length=200, verbose_name='store_name')
+    description = models.TextField(blank=True, null=True)
 
     class Meta:
         ordering = ('name',)
         verbose_name = 'Store'
-        verbose_name_plural = 'Stores'
 
     def __str__(self):
         return self.name
+
+
+class OrderStore(models.Model):
+    """модель поступления товара в магазин со склада"""
+    store = models.ForeignKey(Store, on_delete=models.CASCADE,
+                              related_name='stores')
+    order = models.OneToOneField(ProdStock, on_delete=models.DO_NOTHING,
+                                 related_name='orders')
+    count = models.PositiveBigIntegerField(default=0)
+
 
