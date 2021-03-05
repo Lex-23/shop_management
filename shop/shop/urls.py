d6f0caa5e13d5746c6a901ecf5e0933240c04b8b
Lex-23 <Liverpoolsmile23@gmail.com>
2021-03-05 21:16:05 +0300

    fixed urls auth

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -5,7 +5,7 @@ from users.views import RegisterUserView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api-auth', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
     path('register/', RegisterUserView.as_view(), name='register')
 ]
