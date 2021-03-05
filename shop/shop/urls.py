351f778b070bd6d1f3c99017a0b533d84c587990
Lex-23 <Liverpoolsmile23@gmail.com>
2021-03-05 21:26:45 +0300

    add jwt url

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api-auth', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path('register/', RegisterUserView.as_view(), name='register')
 ]
